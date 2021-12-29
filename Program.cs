519fe10e76ae45f834f42075570811b68abe1aa2
Atacan Kilic <uaatacan@gmail.com>
2021-12-30 01:48:53 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Enter your name: ");
+var yourName = Console.ReadLine();
+Console.WriteLine("Enter your surname: ");
+var yourSurname = Console.ReadLine();
+Console.WriteLine("Hello, " + yourName + " " + yourSurname + ".");
