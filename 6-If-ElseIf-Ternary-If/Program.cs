def6279d26c44b81587c56d9cfc600cf14917701
Atacan Kilic <uaatacan@gmail.com>
2021-12-30 05:23:14 +0300

    karar yapilari 1 eklendi

---
+++ 6-If-ElseIf-Ternary-If/Program.cs
@@ -0,0 +1,19 @@
+﻿int time = DateTime.Now.Hour;
+
+if(time >= 6 && time < 11) {
+    Console.WriteLine("Gunaydin!");
+}
+else if(time <= 18) {
+    Console.WriteLine("Iyi gunler");
+}
+else {
+    Console.WriteLine("Iyi geceler");
+    Console.WriteLine("Iyi geceler");
+    Console.WriteLine("Iyi geceler");
+    Console.WriteLine("Iyi geceler");
+    Console.WriteLine("Iyi geceler");
+}
+
+string sonuc = time >= 6 && time < 11 ? "Gunaydin" : time <= 18 ? "Iyi gunler" : "Iyi Geceler";
+
+Console.WriteLine(sonuc);
