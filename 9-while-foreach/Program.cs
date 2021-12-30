debe410be077808bf70e44bc89b2d7c51b32bd4e
Atacan Kilic <uaatacan@gmail.com>
2021-12-30 06:25:02 +0300

    while-foreach odevi eklendi

---
+++ 9-while-foreach/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Lutfen bir sayi giriniz: ");
+
+int sayi = Convert.ToInt32(Console.ReadLine());
+int sayac = 1;
+int toplam = 0;
+
+while (sayac <= sayi){
+    toplam += sayac;
+    sayac++;
+}
+
+Console.WriteLine(toplam/sayi);
+
+char c = 'a';
+
+while(c < 'z'){
+    Console.WriteLine(c);
+    c++;
+}
+
+string[] arabalar = {"BMW","Ford","Toyota", "Nissan"};
+
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
