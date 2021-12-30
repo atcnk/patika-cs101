6c1b6cfb46b476f7c98343b128ce35b6378c90aa
Atacan Kilic <uaatacan@gmail.com>
2021-12-30 06:04:15 +0300

    for loop odevi eklendi

---
+++ 8-for-loop/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Enter a number: ");
+int counter = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1;i<= counter; i++) {
+    if (i%2 == 1) {
+        Console.WriteLine(i);
+    }
+}
+
+int tekToplam = 0;
+int ciftToplam = 0;
+
+for (int i = 1; i <= 100; i++) {
+    if (i%2 == 1) {
+        tekToplam += i;
+    }
+    else {
+        ciftToplam += i;
+    }
+
+    Console.WriteLine("Tek Toplam: " + tekToplam);
+    Console.WriteLine("Cift Toplam: " + ciftToplam);
+}
+
+for (int i = 1; i < 10; i++) {
+    if (i==4){
+        break;
+    }
+    Console.WriteLine(i);
+}
