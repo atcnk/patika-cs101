c4156a78e3a042c5171f69a6f57c671f7e84e318
Atacan Kilic <uaatacan@gmail.com>
2021-12-30 21:02:54 +0300

    diziler ödevi eklendi

---
+++ 10-diziler/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] renkler = new string[5];
+string[] hayvanlar = {"Kedi", "Maymun", "At"};
+
+int[] dizi = new int[5];
+
+renkler[0] = "Mavi";
+dizi[3] = 10;
+
+Console.WriteLine(hayvanlar[1]);
+Console.WriteLine(dizi[3]);
+Console.WriteLine(renkler[0]);
+
+Console.Write("lutfen dizinin eleman sayisini giriniz: ");
+int diziUzunlugu = Convert.ToInt32(Console.ReadLine());
+int[] sayiDizisi = new int[diziUzunlugu];
+
+for(int i = 0; i<diziUzunlugu; i++) {
+    Console.Write("Lutfen {0}. sayisi giriniz: ", i+1);
+    sayiDizisi[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int toplam = 0;
+
+foreach(var sayi in sayiDizisi){
+    toplam += sayi;
+}
+
+Console.WriteLine("Ortalama: " + toplam/diziUzunlugu);
