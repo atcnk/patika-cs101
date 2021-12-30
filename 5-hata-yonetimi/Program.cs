07525cf3b675cec6a5f938c4828ef4eb5774b928
Atacan Kilic <uaatacan@gmail.com>
2021-12-30 04:43:05 +0300

    hata yonetimi dersi odevi yuklendi

---
+++ 5-hata-yonetimi/Program.cs
@@ -0,0 +1,35 @@
+﻿try {
+    Console.WriteLine("Enter a number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("The number you entered: " + number); 
+}
+
+catch(Exception e){
+    Console.WriteLine("Error: " + e.Message.ToString());
+}
+
+finally {
+    Console.WriteLine("Finished");
+}
+
+try{
+    //int a = int.Parse(null);
+    //int a = int.Parse("test");
+    int a = int.Parse("-20000000000000");
+}
+catch (ArgumentNullException e){
+    Console.WriteLine("You entered null value");
+    Console.WriteLine(e.Message.ToString());
+}
+catch (FormatException e){
+    Console.WriteLine("Data type is not well formatted");
+    Console.WriteLine(e.Message.ToString());
+}
+catch (OverflowException e){
+    Console.WriteLine("You entered too small or too big number");
+    Console.WriteLine(e.Message.ToString());
+}
+
+finally{
+    Console.WriteLine("Finished");
+}
