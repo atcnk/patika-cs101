22c042fd5746dd204d95ab71ad90a3487432ea6d
Atacan Kilic <uaatacan@gmail.com>
2021-12-30 22:10:17 +0300

    metot tanimlama odevi eklendi

---
+++ 12-metot-tanimlama/Program.cs
@@ -0,0 +1,30 @@
+﻿int a = 2;
+int b = 3;
+
+Console.WriteLine(a+b);
+
+int sonuc = Topla(a,b);
+
+Metotlar ornek = new Metotlar();
+ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+int sonuc2 = ornek.ArttirVeTopla(ref a, ref b);
+ornek.EkranaYazdir(Convert.ToString(sonuc2));
+ornek.EkranaYazdir(Convert.ToString(a+b));
+
+int Topla(int deger1, int deger2){
+    return deger1 + deger2;
+}
+
+class Metotlar {
+    public void EkranaYazdir(string veri) {
+        Console.WriteLine(veri);
+    }
+
+    public int ArttirVeTopla(ref int deger1, ref int deger2) {
+        deger1++;
+        deger2++;
+        return deger1 + deger2;
+    }
+}
+
