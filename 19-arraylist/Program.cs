d2f239027a42f46b25651b743cf4e7edd8112a67
Atacan Kilic <uaatacan@gmail.com>
2022-01-01 00:25:15 +0300

    arraylist odevi yuklendi

---
+++ 19-arraylist/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+ArrayList liste = new ArrayList();
+// liste.Add("Ayse");
+// liste.Add(2);
+// liste.Add(true);
+// liste.Add('A');
+
+// Console.WriteLine(liste[1]);
+
+// foreach(var item in liste) { Console.WriteLine(item);}
+
+List<int> sayilar = new List<int>(){1,8,4,7,9,92,5};
+
+liste.AddRange(sayilar);
+
+foreach (var item in liste)
+{
+    Console.WriteLine(item);
+}
+
+liste.Sort();
+
+foreach (var item in liste) {
+    Console.WriteLine(item);
+}
+
+Console.WriteLine(liste.BinarySearch(9));
+
+liste.Reverse();
+
+foreach (var item in liste)
+{
+    Console.WriteLine(item);
+}
+
+liste.Clear();
+
+foreach(var item in liste) {
+    Console.WriteLine(item);
+}
