79f13e7c3ca3bc340367e2978b4e8faa701ca4f5
Atacan Kilic <uaatacan@gmail.com>
2022-01-01 20:16:39 +0300

    sinif, kurucu, encapsulation, static, enum odevleri eklendi

---
+++ 21-sinif/Program.cs
@@ -0,0 +1,28 @@
+﻿Calisan calisan1 = new Calisan();
+calisan1.Ad = "Ayşe";
+calisan1.Soyad = "Kara";
+calisan1.No = 23425634;
+calisan1.Departman = "İnsan Kaynakları";
+calisan1.CalisanBilgileri();
+
+Calisan calisan2 = new Calisan();
+calisan2.Ad = "Deniz";
+calisan2.Soyad = "Arda";
+calisan2.No = 25646789;
+calisan2.Departman = "Satın Alma";
+calisan2.CalisanBilgileri();
+
+class Calisan 
+{
+    public string Ad;
+    public string Soyad;
+    public int No;
+    public string Departman;    
+    public void CalisanBilgileri()
+    {
+        Console.WriteLine("Çalışan Adı: {0}", Ad);
+        Console.WriteLine("Çalışan Soyadı: {0}", Soyad);
+        Console.WriteLine("Çalışan No: {0}", No);
+        Console.WriteLine("Çalışan Departmanı: {0}", Departman);
+    }
+}
---
+++ 25-enum/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine(Gunler.Pazar);
+
+Console.WriteLine((int)Gunler.Cumartesi);
+
+int sicaklik = 32;
+
+if(sicaklik <= (int)HavaDurumu.Normal)
+{
+    Console.WriteLine("Dışarıya çıkmak için havanın ısınmasını bekleyin.");
+}
+else if (sicaklik >= (int)HavaDurumu.Sıcak)
+{
+    Console.WriteLine("Dışarıya çıkmak için çok sıcak.");
+}
+else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik < (int)HavaDurumu.CokSıcak)
+{
+    Console.WriteLine("Dışarı çıkabilirsiniz.");
+}
+
+enum Gunler
+{
+    Pazartesi = 1,
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma = 23,
+    Cumartesi,
+    Pazar
+}
+
+enum HavaDurumu
+{
+    Soguk = 5,
+    Normal = 20,
+    Sıcak = 25,
+    CokSıcak = 30
+}
